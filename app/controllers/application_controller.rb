4709d1a8c89184652f2046795d43d06cd3bec48f
ryuta26 <fururyu0504@docomo.ne.jp>
2020-02-26 15:31:47 +0900

    error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_kana, :first_kana, :postal_code, :domicile, :phone_number])
+  end
+
+  def after_sign_in_path_for(resource)
+  case resource
+  when Admin
+    admin_items_path
+  when EndUser
+    end_user_path
+  end
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :end_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+  	resources :items, only: [:index]
+  end
+
+  resources :end_users, only: [:show]
 end
